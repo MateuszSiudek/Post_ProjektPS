33c90a4478f2d29c65d53df736c4e2c2cdd210d8
MateuszSiudek <mateusz.siudek@student.up.krakow.pl>
2025-01-27 00:18:14 +0100

    Add files via upload

--- src/main/java/com/example/jsonplaceholderapi/controller/PostController.java
+++ src/main/java/com/example/jsonplaceholderapi/controller/PostController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,7 +41,7 @@ public class PostController {
         return postService.updatePost(id, updatedPost);
     }
 
-    @PutMapping
+    @PostMapping
     public Post addPost(@RequestBody Post newPost) {
         return postService.addPost(newPost);
     }
